020391cf399b0e51d3eaceb6610dbf20eec51f2b
Michael Angerman <stormasm@gmail.com>
2017-02-21 14:01:10 -0800

    reading json file

--- tmult.js
+++ tmult.js
@@ -1,5 +1,17 @@
+fs = require('fs');
 var Web3 = require('web3');
 
 var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 var number = web3.eth.blockNumber;
-console.log(number)
+//console.log(number)
+
+fs.readFile('tmult.json', 'utf8', function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  var obj = JSON.parse(data);
+  abi = obj.contracts['tmult\.sol:TMult'].abi;
+  bin = obj.contracts['tmult\.sol:TMult'].bin;
+  console.log(abi)
+  console.log(bin)
+});
